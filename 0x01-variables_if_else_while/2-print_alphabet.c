b58d6bf8f290b022603951b995bd07ddfbf01363
tony <tony@git.com>
2022-06-03 06:19:11 -0700

    added 2-print_alphabet solution

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - programs starts here
+ *
+ * this program uses  putchar to print all the english alphabet
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char ch;
+
+	ch = 'a';
+	while (ch < 123)
+	{
+		putchar(ch);
+		putchar('\n');
+		ch++;
+	}
+	return (0);
+}
