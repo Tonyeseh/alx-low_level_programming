e261deea6fa0222e7e39155473670173fce21d07
tony <tony@git.com>
2022-06-03 15:32:58 -0700

    added 4-print-alphabt.c again

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,28 +1,24 @@
 #include <stdio.h>
-
 /**
- * main - programs starts here
+ * main - program starts here
  *
- * this program uses  putchar to print all the english alphabet except q and e
+ * displays all alphabets lower case save q and e
  *
  * Return: 0
  */
-int main(void)
-{
-	char ch;
 
-	ch = 'a';
-	while (ch <= 'z')
+int main() {
+    // Write C code here
+    int c = 'a';
+    
+    while (c <= 'z')
+    {
+    	if (c != 'q' && c != 'e')
 	{
-		if (ch != 'q' && ch != 'e')
-		{
-			putchar(ch);
-		}
-		ch++;
-
+		putchar(c);
+        	c++;
 	}
-
-	putchar('\n');
-
-	return (0);
+    }
+    putchar('\n');
+    return 0;
 }
