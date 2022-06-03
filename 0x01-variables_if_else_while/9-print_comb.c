4fe8600f9d3a6ca1ab802da58e5bbe05e912e83f
tony <tony@git.com>
2022-06-03 16:10:41 -0700

    added 9-print_comb.c solution

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - program starts here
+ *
+ * prints all possible combinations of single-digit numbers using only putchar
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char ch;
+
+	ch = '0';
+	while (ch <= '9')
+	{
+		putchar(ch);
+
+		if (ch != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
