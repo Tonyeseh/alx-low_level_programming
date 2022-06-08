8f5e0d4c34e34ccec4cbae8f0cca73cb2897aeb1
tony <tony@git.com>
2022-06-07 18:12:54 -0700

    added solution to print 98 function file and added the prototype to main.h

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,48 +1,20 @@
 #include "main.h"
 
 /**
- * print_to_98 - prints all natural numbers from n to 98
- * followed by a new line.
+ * less_than_98 - prints numbers less than 98
  *
  * @n: parameter passed
  *
- * Description: numbers must be separated by a comma, followed by a space
- * numbers should be printed in order
- * first printed number should be the on passed to your function
- * last printed number should be 98
- *
- * Return: list of numbers.
+ * Return: void
  */
-void print_to_98(int n)
+void less_than_98(int n)
 {
-	while (n > 98)
-	{
-		if (n >= 100)
-		{
-			_putchar((n / 100) + '0');
-			_putchar(((n / 10) % 10) + '0');
-			_putchar((n % 10) + '0');
-		}
-		else
-		{
-			_putchar((n / 10) + '0');
-			_putchar((n % 10) + '0');
-		}
-		_putchar(',');
-		_putchar(' ');
-		n--;
-		if (n == 98)
-		{
-			_putchar((n / 10) + '0');
-			_putchar((n % 10) + 48);
-			n = 99;
-			break;
-		}
-	}
 	while (n <= 98)
 	{
 		if (n < 0)
 		{
+			int p;
+
 			_putchar('-');
 			p = n * -1;
 			if ((p / 10) == 0)
@@ -80,3 +52,64 @@ void print_to_98(int n)
 	}
 }
 
+/**
+ * greater_than_98 - prints numbers greater than 98
+ *
+ * @n: parameter passed
+ *
+ * Return: void
+ */
+
+void greater_than_98(int n)
+{
+	while (n > 98)
+	{
+		if (n >= 100)
+		{
+			_putchar((n / 100) + '0');
+			_putchar(((n / 10) % 10) + '0');
+			_putchar((n % 10) + '0');
+		}
+		else
+		{
+			_putchar((n / 10) + '0');
+			_putchar((n % 10) + '0');
+		}
+		_putchar(',');
+		_putchar(' ');
+		n--;
+		if (n == 98)
+		{
+			_putchar((n / 10) + '0');
+			_putchar((n % 10) + 48);
+			n = 99;
+			break;
+		}
+	}
+}
+
+/**
+ * print_to_98 - prints all natural numbers from n to 98
+ * followed by a new line.
+ *
+ * @n: parameter passed
+ *
+ * Description: numbers must be separated by a comma, followed by a space
+ * numbers should be printed in order
+ * first printed number should be the on passed to your function
+ * last printed number should be 98
+ *
+ * Return: list of numbers.
+ */
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		less_than_98(n);
+	}
+	else
+	{
+		greater_than_98(n);
+	}
+	_putchar('\n');
+}
