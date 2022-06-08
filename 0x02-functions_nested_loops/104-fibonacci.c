6cc4701a967d9136b3cd9c5572784203942303ff
tony <tony@git.com>
2022-06-07 21:53:42 -0700

    added 104-fibonacci.c solution

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - prints the first 98 fibonacci numbers
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	long unsigned int tmp, new, prev;
+	int n;
+
+	n = 1;
+	new = 2;
+	prev = 1;
+	printf("%lu, %lu, ", prev, new);
+	while (n <= 98)
+	{
+		tmp = new;
+		new = new + prev;
+		prev = tmp;
+		printf("%lu", new);
+		n++;
+		if (n != 99)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+	return (0);
+}
