24c1651ce3f2548435ee004c85a4723b44f30926
tony <tony@git.com>
2022-06-07 21:02:48 -0700

    added 102-fibonacci.c solution

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - prints the first 50 fibonacci numbers
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	long int tmp, prev, new;
+	int n;
+
+	n = 0;
+	prev = 1;
+	new = 2;
+	printf("%ld, %ld, ", prev, new);
+
+	while (n <= 48)
+	{
+		tmp = new;
+		new = new + prev;
+		prev = tmp;
+		printf("%ld", new);
+		if (n != 48)
+		{
+			printf(", ");
+		}
+		n++;
+	}
+	printf("\n");
+	return (0);
+}
