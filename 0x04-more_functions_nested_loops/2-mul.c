d14c015de01a3444b0b719628296f61562842a2b
tony <tony@git.com>
2022-06-09 04:38:42 -0700

    added solution to 0x04 task 2 (2-mul.c) and modified the header file main.h

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - multiplies 2 integers
+ *
+ * @a: first integer passed
+ * @b: second integer passed
+ *
+ * Description: returns the product of the 2 integer passed
+ *
+ * Return: the product of a and b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
