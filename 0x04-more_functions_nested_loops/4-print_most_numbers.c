247e94651e91c8d46f34fd3088691dfdd572f9bb
tony <tony@git.com>
2022-06-09 05:02:08 -0700

    added solution to 0x04 task 4(4-print_most_numbers.c)

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints all numbers 0 -9 except 2 and 4
+ *
+ * Return: void
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	i = '0';
+	while (i <= '0' && i >= '9')
+	{
+		if (i != '2' || i != '4')
+		{
+			_putchar(i);
+		}
+		i++;
+	}
+	_putchar('\n');
+}
