bf348b774d5755080babb100256a515993b4d44f
tony <tony@git.com>
2022-06-09 05:14:35 -0700

    corrected output of 0x04 task 4 solution

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -11,9 +11,9 @@ void print_most_numbers(void)
 	int i;
 
 	i = '0';
-	while (i <= '0' && i >= '9')
+	while (i <= '9')
 	{
-		if (i != '2' || i != '4')
+		if (i != '2' && i != '4')
 		{
 			_putchar(i);
 		}
