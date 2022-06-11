d922bd43ddc093b0784020c47d1541cb5d39b94b
tony <tony@git.com>
2022-06-11 14:12:48 -0700

    copied this answer

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,46 +1,37 @@
 #include <stdio.h>
-
+#define LARGEST 10000000000
 /**
- * main - prints the first 98 fibonacci numbers
- *
+ * main - main block
+ * Description: Find and print the first 98 fib numbers starting with 1 and 2.
+ * Numbers should be coma and space separated.
  * Return: 0
  */
-
 int main(void)
 {
-	unsigned long int hold1, hold2, hold3, newf, prevf, newb, prevb;
-	int n;
+	unsigned long int frnum1 = 0, bknum1 = 1, frnum2 = 0, bknum2 = 2;
+	unsigned long int hold1, hold2, hold3;
+	int count;
 
-	n = 3;
-	newb = 2;
-	prevb = 1;
-	newf = prevf = 0;
-	printf("%lu, %lu, ", prevb, newb);
-	while (n <= 98)
+	printf("%lu, %lu, ", bknum1, bknum2);
+	for (count = 2; count < 98; count++)
 	{
-		if (newb + prevb > 1000000000000000 || newf > 0 || prevf > 0)
+		if (bknum1 + bknum2 > LARGEST || frnum2 > 0 || frnum1 > 0)
 		{
-			hold1 = (newb + prevb) / 1000000000000000;
-			hold2 = (newb + prevb) % 1000000000000000;
-			hold3 = newf + prevf + hold1;
-			prevf = newf;
-			newf = hold3;
-			prevb = newb;
-			newb = hold2;
-			printf("%lu%10lu", newf, newb);
+			hold1 = (bknum1 + bknum2) / LARGEST;
+			hold2 = (bknum1 + bknum2) % LARGEST;
+			hold3 = frnum1 + frnum2 + hold1;
+			frnum1 = frnum2, frnum2 = hold3;
+			bknum1 = bknum2, bknum2 = hold2;
+			printf("%lu%010lu", frnum2, bknum2);
 		}
 		else
 		{
-			hold1 = newb;
-			newb = newb + prevb;
-			prevb = hold1;
-			printf("%lu", newb);
+			hold2 = bknum1 + bknum2;
+			bknum1 = bknum2, bknum2 = hold2;
+			printf("%lu", bknum2);
 		}
-		n++;
-		if (n != 99)
-		{
+		if (count != 97)
 			printf(", ");
-		}
 	}
 	printf("\n");
 	return (0);
