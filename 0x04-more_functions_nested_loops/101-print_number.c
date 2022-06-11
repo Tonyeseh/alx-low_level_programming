8bd9c578b71a0458e7beef93fc6229a28616f9af
tony <tony@git.com>
2022-06-11 12:00:15 -0700

    solution to task 12 0x04-more_functions
    added file 101-print_number that prints any integer and added its
    prototype to main.h

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer
+ *
+ * @n: the integer to be printed
+ *
+ * Description - uses _putchar to print integer
+ *
+ * Return: void
+ */
+
+void print_number(int n)
+{
+	int d, num;
+
+	d = 1;
+
+	/* check if number is negative, prints - sign and makes it positive */
+	if (n < 0)
+	{
+		_putchar('-');
+		n = n * -1;
+	}
+
+	num = n;
+
+	while (num >= 10)
+	{
+		d *= 10;
+		num /= 10;
+	}
+
+	while (d >= 1)
+	{
+		_putchar(((n / d) % 10) + '0');
+		d /= 10;
+	}
+}
