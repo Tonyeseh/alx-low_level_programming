3ec94c7a76bc01bcdea02e59ffd1d75910a55563
tony <tony@git.com>
2022-06-16 06:56:33 -0700

    soluion to task 2 0x06-pointers_arrays_strings
    added the function prototype in 2-strncpy to main.h

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies n number of bytes from src to dest string
+ *
+ * @dest: destination string
+ * @src: source string
+ *
+ * Return: pointer to destnationstion string
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int j, i;
+
+	i = j = 0;
+
+	while (i < n)
+	{
+		if (src[j] == '\0')
+		{
+			dest[i] = '\0';
+		}
+		else
+		{
+			dest[i] = src[j];
+			j++;
+		}
+		i++;
+	}
+	return (dest);
+}
