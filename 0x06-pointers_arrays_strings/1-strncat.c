d93a0c01daea9e10b0901663e10920038c8807ec
tony <tony@git.com>
2022-06-16 06:44:12 -0700

    solution to task 1 0x06-functions_arrays_strings
    added the prototype of the function in 1-strncat to main.h

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates two strings with most n bytes from src
+ *
+ * @dest: destination string
+ * @src: source string
+ * @n: number of bytes to be copied
+ *
+ * Return: pointer to destionation string
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int j, full, len;
+
+	len = j = full = 0;
+	while (dest[len] != '\0')
+	{
+		len++;
+	}
+	while (n > full - 1)
+	{
+		if (src[j] == '\0')
+		{
+			dest[len] = '\0';
+		}
+		else
+		{
+			dest[len] = src[j];
+			j++;
+		}
+		len++;
+		full++;
+	}
+	return (dest);
+}
