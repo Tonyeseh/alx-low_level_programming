24d4cf2cf0aace2a2b313c5293ce3894a5c2278d
tony <tony@git.com>
2022-06-18 05:04:46 -0700

    solution to task 9 0x06-pointers_arrays_strings
    added the prototype of the function in 101-print_number.c to main.h

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_number - prints an integer with _putchar function
+ *
+ * @n: number to be printed
+ *
+ * Return: Nothing
+ */
+
+void print_number(int n)
+{
+	int place, d;
+
+	d = 1;
+	if (n < 0)
+	{
+		_putchar('-');
+		n = n * -1;
+	}
+
+	place = n;
+	while ((place) >= 10)
+	{
+		place = place / 10;
+		d *= 10;
+	}
+
+	while (d > 0)
+	{
+		_putchar(((n / d) % 10) + '0');
+		d /= 10;
+	}
+}
