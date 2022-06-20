31937366978351e01b7decd5eed1a1dbfac94f54
tony <tony@git.com>
2022-06-20 07:08:46 -0700

    solution to task 3 0x07-pointers_arrays_strings
    added the prototype of the function in 3-strspn.c to main.h

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * _strspn - finds the number of bytes in s that are in accept
+ * stops when it meets a character not in accept
+ *
+ * @s: string
+ * @accept: the strings to be accepted
+ *
+ * Return: number of bytes only from accept
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, j;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		j = 0;
+		while (accept[j] != '\0')
+		{
+			if (s[i] == accept[j])
+			{
+				i++;
+				break;
+			}
+			if (accept[j + 1] == '\0')
+			{
+				return (i);
+			}
+			j++;
+		}
+	}
+	return (i);
+}
