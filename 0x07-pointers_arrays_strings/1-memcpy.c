f6626111d41259defccbbf0f8cc9bd363c1713dc
tony <tony@git.com>
2022-06-20 06:53:04 -0700

    fixed 1-memcpy.c betty error

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -7,7 +7,7 @@
  * @src: memory to copy bytes from
  * @n: number of byes to copy from src to dest
  *
- * Return: dest\
+ * Return: dest
  */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
