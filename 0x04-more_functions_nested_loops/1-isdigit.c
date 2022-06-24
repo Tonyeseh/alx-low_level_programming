60b5a65876dda131ba7af7d1de2b2cee5d09f1d7
Tonyeseh <88238621+Tonyeseh@users.noreply.github.com>
2022-06-24 23:38:03 +0100

    reset the changes done to 1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,19 +1,18 @@
 #include "main.h"
 
 /**
- * _isupper - tells if the character passed is an uppercase letter
+ * _isdigit - checks if a int c passed is a digit in ascii standard
  *
- * @c: the character passed
+ * @c: int passed
  *
- * Description: returns 1 if the cahracter is an uppercase letter
- * and 0 if it is a lowercase character
+ * Description: returns 1 if c passed is a digit and 0 if it's not
  *
  * Return: 0 or 1
  */
 
-int _isupper(int c)
+int _isdigit(int c)
 {
-	if (c >= 65 && c <= 90)
+	if (c >= '0' && c <= '9')
 	{
 		return (1);
 	}
