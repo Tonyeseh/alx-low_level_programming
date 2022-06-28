c3a57c15c91d400a0c72d7c0f533633c85e0b237
tony <tony@git.com>
2022-06-28 01:49:34 -0700

    soltion to task 1 0x0B
    works like strdup function, prototype is declared in main.h

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _strdup - copies the string to another momery space in the heap section
+ *
+ * @str: string passed
+ *
+ * Return: NULL or pointer to the string
+ */
+
+char *_strdup(char *str)
+{
+	char *ptr;
+	int i, len;
+
+	len = strlen(str);
+	ptr = (char *) malloc((len + 1) * sizeof(char));
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < len; i++)
+	{
+		ptr[i] = str[i];
+	}
+
+	*(ptr + len) = '\0';
+	return (ptr);
+}
