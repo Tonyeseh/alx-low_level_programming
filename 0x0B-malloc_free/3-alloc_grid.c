527ee329978176a376c0d8d05ed0ee1ab4657297
tony <tony@git.com>
2022-06-28 02:22:54 -0700

    solution to task 3 0x0B
    the prototype to the function is in main.h

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - elements of the grid should be initialised to 0
+ *
+ * @width: width
+ * @height: height
+ *
+ * Return: NULL if height or width is 0 or negative or pointer
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **ptr;
+	int i, j;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+
+	ptr = (int **) malloc(height * sizeof(int *));
+	for (i = 0; i < height; i++)
+	{
+		*(ptr + i) = (int *) malloc(width * sizeof(int));
+	}
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			ptr[i][j] = 0;
+		}
+	}
+	return (ptr);
+}
