f339a973efdc547559a2410b1f6657ce638dc690
tony <tony@git.com>
2022-07-01 18:58:13 -0700

    initialised the memory allocared by calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,14 +12,21 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *ptr;
+	char *ptr;
+	unsigned int i;
 
-	if (nmemb <= 0 || size <= 0 )
+	if (nmemb <= 0 || size <= 0)
 		return (NULL);
 
 	ptr = malloc(nmemb * size);
 	if (ptr == NULL)
 		return (NULL);
 
+	i = 0;
+	while (i < (nmemb * size))
+	{
+		ptr[i] = '\0';
+		i++;
+	}
 	return (ptr);
 }
