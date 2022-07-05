20183fe748e8bfc84cccefe2e4ef0a2b2399580f
tony <tony@git.com>
2022-07-05 03:03:05 -0700

    solution to task 0-4 in 0x0D-preprocessor lowlevelP
    added the test files in test folder

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of the file is was compiles from and a new line
+ *
+ * Return: Always 0.
+ *
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
