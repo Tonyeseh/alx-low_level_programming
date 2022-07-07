c1f520896378c4d4b8de7672bf801f1dd9cc2821
tony <tony@git.com>
2022-07-07 02:55:51 -0700

    corrected betty syntax and added check if f is NULL in 0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,14 +1,19 @@
 #include "function_pointers.h"
+#include <stddef.h>
 
 /**
  * print_name - prints a name
  *
  * @name: name to be printed
+ * @f: pointer to a function passed
  *
  * Return: Nothing
  */
 
 void print_name(char *name, void (*f)(char *))
 {
-	f(name);
+	if (f != NULL)
+	{
+		f(name);
+	}
 }
