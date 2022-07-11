f2ccce4fd0dc1f3c38e88a971d3813a394908caf
tony <tony@git.com>
2022-07-11 09:06:09 -0700

    added 3-get_op_func.c

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,36 @@
+#include "3-calc.h"
+#include <stddef.h>
+
+/**
+ * get_op_func - gets the operation to be carried out on two operands
+ *
+ * @s: string format of the operand
+ *
+ * Return: a pointer to the function that does the operation or
+ * NULL
+ */
+
+
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
+
+	i = 0;
+	while (i < 5)
+	{
+		if (*ops[i].op == *s)
+		{
+			return ops[i].f;
+		}
+		i++;
+	}
+	return (NULL);
+}
