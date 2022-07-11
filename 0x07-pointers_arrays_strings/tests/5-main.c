c539fd10d3045969a2c725b0ef14f0f9511e475a
tony <tony@git.com>
2022-07-10 18:07:23 -0700

    added tests files for the project 0x07-pointers_arrays_strings LLP

---
+++ 0x07-pointers_arrays_strings/tests/5-main.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char *s = "hello, world";
+    char *f = "world";
+    char *t;
+
+    t = _strstr(s, f);
+    printf("%s\n", t);
+    t = _strstr("First, solve the problem. Then, write the code.", "solve");
+    printf("%s\n", t);
+    _strstr("First, solve the problem. Then, write the code.", "School");
+    printf("%s\n", t);
+    _strstr("First, solve the problem. Then, write the code.", "");
+    printf("%s\n", t);
+    return (0);
+}
