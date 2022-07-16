15b73576c99d20f903f9bb97d91812a02844f7fb
tony <tony@git.com>
2022-07-15 20:50:37 -0700

    solution to task 0 0x12-singly_linked_lists LLP
    header file lists.h contains all the function prototype

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_list - prints all the elements of a list_t list
+ *
+ * Description - if st is NULL print [0] (nil)
+ * 
+ * @h: pointer to list_h struct
+ *
+ * Return: numbe of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t nodes;
+
+	nodes = 0;
+	while (h)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+
+		else
+			printf("[%d] %s\n", h->len, h->str);
+
+		nodes++;
+		h = h->next;
+	}
+	return (nodes);
+}
