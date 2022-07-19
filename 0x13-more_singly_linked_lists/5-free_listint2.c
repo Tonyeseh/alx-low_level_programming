659096d56125704f57ca4160e0aa5ededa1da938
tony <tony@git.com>
2022-07-19 07:38:24 -0700

    solution to task 5 0x13-sngly_linked_list LLP

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a listint_t list
+ *
+ * @head: pointer to the first node of a lintint_t list
+ *
+ * Description: frees a listint_t list and sets head to NULL
+ *
+ * Return: Nothing
+ */
+
+
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp, *node;
+
+	if (*head != NULL)
+	{
+		tmp = *head;
+		while (tmp != NULL)
+		{
+			node = tmp;
+			tmp = tmp->next;
+			free(node);
+		}
+		*head = NULL;
+	}
+}
