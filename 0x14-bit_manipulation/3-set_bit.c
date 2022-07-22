250e27144f52366ca71f6ac47ff9ddaec63eea80
tony <tony@git.com>
2022-07-22 06:26:39 -0700

    betty correction to 3-set_bit.c

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -3,6 +3,7 @@
 /**
  * set_bit - sets the value of a bit to 1 at a given index
  *
+ * @n: pointer to number n
  * @index: is the index starting from 0 of the bit you want to set
  *
  * Return:1 if worked, or -1 if an error occured
