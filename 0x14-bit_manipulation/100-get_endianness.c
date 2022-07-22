d60d219c36fb2133d68068b4a9ee66406a7eb966
tony <tony@git.com>
2022-07-22 11:20:57 -0700

    solution to task 6 0x14-bit_manipulation LLP

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * get_endianness - checks the endianness
+ *
+ * Return: 0 if big endian and 1 if little endian
+ */
+
+
+int get_endianness(void)
+{
+	unsigned int n = 1;
+	char *ptr_n;
+
+	ptr_n = (char *)&n;
+
+	if (*ptr_n == 1)
+		return (1);
+	else
+		return (0);
+}
