0952f56b82cae0954f4897e01342957bc756d3d1
tony <tony@git.com>
2022-07-26 08:53:41 -0700

    corrected betty style and documentation error

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -6,7 +6,7 @@
  * @filename: name of the file
  * @text_content: is the NULL terminated string to add at the end of the file
  *
- * @Description: if text_content is NULL do not add anything to the file
+ * Description: if text_content is NULL do not add anything to the file
  * return 1 if the file exists and -1 if the file does not of if you don't
  * have the required permissions to write the file.
  * does not create the file if it does not exist
@@ -32,6 +32,6 @@ int append_text_to_file(const char *filename, char *text_content)
 	}
 	if (o == -1 || w == -1)
 		return (-1);
-	close (o);
+	close(o);
 	return (1);
 }
