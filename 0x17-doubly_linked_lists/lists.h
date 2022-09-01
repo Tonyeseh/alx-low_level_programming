1503aaba508ee19eea04ce1d3a904f13eaea55bc
tony <tony@git.com>
2022-09-01 08:17:50 -0700

    added header file for 0x17 LLL

---
+++ 0x17-doubly_linked_lists/lists.h
@@ -0,0 +1,31 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+/**
+ * struct dlistint_s - doubly linked list
+ * @n: integer
+ * @prev: points to the previous node
+ * @next: points to the next node
+ *
+ * Description: doubly linked list node structure
+ *
+ */
+
+/* Standard Lib */
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Struct Declaration */
+typedef struct dlistint_s
+{
+	int n;
+	struct dlistint_s *prev;
+	struct dlistint_s *next;
+} dlistint_t;
+
+/* My Function */
+size_t print_dlistint(const dlistint_t *h);
+
+
+#endif
