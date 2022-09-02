13d9125eaa1578465467184db7434dd2ebcd8075
tony <tony@git.com>
2022-09-02 04:13:13 -0700

    made task 7 0x17 HLP to insert at the right place

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -21,34 +21,31 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		head = head->next;
 	}
 
-	if (idx <= size)
+	if (idx <= size + 1)
 	{
-		new_node = malloc(sizeof(dlistint_t));
-		head = *h;
-		if (!new_node)
-			return (NULL);
-		new_node->n = n;
-		while (i < idx)
+		if (idx == 0)
 		{
-			i++;
-			head = head->next;
+			new_node = add_dnodeint(h, n);
 		}
-		if (size == 0)
+		else if (idx == size + 1)
 		{
-			new_node->prev = NULL;
-			new_node->next = NULL;
-			*h = new_node;
-		}
-		else if (size == idx)
-		{
-			new_node->next = NULL;
-			new_node->prev = head;
-			head->next = new_node;
+			new_node = add_dnodeint_end(h, n);
 		}
 		else
 		{
+			head = *h;
+			while (i < (idx - 1))
+			{
+				head = head->next;
+				i++;
+			}
+			new_node = malloc(sizeof(dlistint_t));
+			if (!new_node)
+				return (NULL);
+			new_node->n = n;
 			new_node->next = head->next;
 			new_node->prev = head;
+			head->next->prev = new_node;
 			head->next = new_node;
 		}
 		return (new_node);
